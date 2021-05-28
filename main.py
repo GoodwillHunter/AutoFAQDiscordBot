92b83b21d15843bf91429cc611905e339c6c023e
GoodwillHunter <>
2021-05-28 19:30:36 +0000

    Question answering capability added.

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 """
-We present to you a discord bot that can help your community by answersing comman questions from newcomers saving time of older members from answering repetitive questions.
+We present to you a discord bot that can help your community by answersing common questions from newcomers saving time of older members from answering repetitive questions.
 
 The FAQ is managed automatically so its up-to-date as time passes eliminating the need for manually maintaining the FAQ.
 
@@ -13,6 +13,20 @@ Salient Features include
 
 
 import discord
+import os
+import datetime
+from replit import db
+import sys
+import subprocess
+try:
+  from Levenshtein import ratio
+except:
+  subprocess.check_call([sys.executable, '-m', 'pip', 'install',
+'python-levenshtein'])
+  from Levenshtein import ratio
+
+
+client = discord.Client()
 
 """
 Important Links:
@@ -31,14 +45,24 @@ Important Links:
 
 
 """
+question_words = set(["anyone", 'tell','can','what','where','when','how','which','who','why','suggest'])
+# TODO: make utility functions for question_words
 
+#TDOD: Make created_at channel wise and add to db
+created_at = None
+threshold = 0.5
+# TODO: Make utility functions for threshold
 
 def isQuestion(msg):
   # Detect if the msg is a question or not
   # return boolean
-  pass
+  msg = msg.lower()
+  if any(word in msg for word in question_words):
+      return True
+  return False
 
-def getAnswer(ques):
+def getAnswer(ques, arr):
+  # phase 2
   # for a given question, find the answer
   # the answer could be a:
   #     Reply to the question
@@ -46,25 +70,113 @@ def getAnswer(ques):
   #     The very next msg
   pass
 
-def getChatHistory():
+async def getChatHistory(channel):
   # get chat history of a given text channel
   # https://discordpy.readthedocs.io/en/stable/api.html#discord.TextChannel.history
-  pass
+  # print("getChatHistory called")
+  temp = []
+  global created_at
+  async for message in channel.history(limit=None, after=created_at):
+    temp.append(message)
+  created_at = datetime.datetime.now()
+  arr = []
+  n = len(temp)
+  m = n-1
+  for i in range(n):
+    arr.append(temp[m])
+    m -= 1
+
+  return arr;
 
 def add(question, answer):
+  # TODO: Divide the question sentence wise and only add question part.
+  channel = str(question.channel)
+  if(channel not in db):
+    db[channel] = {}
   # adds question-answer pair into the database
-  pass
+  print("Q. "+question.content)
+  print("A. "+answer.content)
+  max_score = 0
+  for ques2 in db[channel]:
+    match = ratio(ques2, question.content)
+    if(match>max_score):
+      max_score = match
+  if(max_score < threshold):
+    db[channel][question.content.lower()] = answer.content
 
-def generateFAQ():
+async def generateFAQ(channel):
   # used self.last_generated- datetime
   # runs initially and then every 24 hrs
   # iterates through chat history and find question-answer pairs
   # put them in a database
-  pass
+  # print("generateFAQ called")
+  channel1 = str(channel)
+  unanswered_questions = {}
+  if(channel1 not in db):
+    db[channel1] = {}
+  if("unanswered_questions" in db[channel1]):
+    unanswered_questions = db[channel1]["unanswered_questions"]
+  arr = await getChatHistory(channel)
+  for msg in arr:
+    if msg.author == client.user:
+      continue
+    if(isQuestion(msg.content)):
+      unanswered_questions[msg.id] = msg
+    if(msg.reference!=None and msg.reference.message_id in unanswered_questions and unanswered_questions[msg.reference.message_id].author!=msg.author):
+      add(unanswered_questions[msg.reference.message_id], msg)
+      unanswered_questions.pop(msg.reference.message_id)
+  # db[channel1]["unanswered_questions"] = unanswered_questions
 
 def generateFAQfromExisting(faq):
   # adds existing question-answers pair into the database
   # max score for these answers
+  # TODO
+  pass
+
+
+# def getApproximateAnswer2(q):
+#     max_score = 0
+#     answer = ""
+#     prediction = ""
+#     for idx, row in data.iterrows():
+#         score = ratio(row["Question"], q)
+#         if score >= 0.9: # I'm sure, stop here
+#             return row["Answer"], score, row["Question"]
+#         elif score > max_score: # I'm unsure, continue
+#             max_score = score
+#             answer = row["Answer"]
+#             prediction = row["Question"]
+#     if max_score > 0.3: # threshold is lowered
+#         return answer, max_score, prediction
+#     return "Sorry, I didn't get you.", max_score, prediction
+
+async def answer(ques):
+  # find the matching question and confidence
+  # if confidence > threshold, writes answer on the text channel
+  channel = str(ques.channel)
+  max_score = 0
+  answer = ""
+  prediction = ""
+  for ques2 in db[channel]:
+    match = ratio(ques2, ques.content)
+    if(match>max_score):
+      max_score = match
+      answer = db[channel][ques2]
+      prediction = ques2
+  if(max_score >= threshold):
+    await ques.channel.send("Hey, your friendly FAQ Bot here. Here is a related question asked befor: "+prediction)
+    await ques.channel.send("Here is the answer: "+answer)
+
+def feedback(feedback):
+  # handles feedback for answer output
+  # adjusts score accordingly
+  # TODO
+  pass
+
+def botReplySwitch(user):
+  # switches bot reply for specific user
+  # if no user provided, then acts as master on/off switch for the bot
+  # TODO
   pass
 
 def maintainDB():
@@ -74,17 +186,33 @@ def maintainDB():
   # sorts based on importance
   pass
 
-def answer(ques):
-  # find the matching question and confidence
-  # if confidence > threshold, writes answer on the text channel
-  pass
+@client.event
+async def on_ready():
+  print('We have logged in as {0.user}'.format(client))
 
-def feedback(feedback):
-  # handles feedback for answer output
-  # adjusts score accordingly
-  pass
+@client.event
+async def on_message(message):
+  # if created_at + 24 hrs > datetime.now
+  #   await generateFAQ(message.channel)
+  global created_at
+  # if(created_at==None or created_at+datetime.timedelta(hours=24)<datetime.datetime.now()):
+  #   await generateFAQ(message.channel)
+  if message.author == client.user:
+    return
 
-def botReplySwitch(user):
-  # switches bot reply for specific user
-  # if no user provided, then acts as master on/off switch for the bot
-  pass
+  msg = message.content
+
+  if(isQuestion(msg)):
+    await answer(message)
+
+  if msg.startswith('$generateFAQ'):
+    # print("command recieved")
+    channel1 = str(message.channel)
+    if(channel1 in db):
+      db.pop(channel1)
+    created_at = None
+    await generateFAQ(message.channel)
+    print("FAQ reset successfull")
+    await message.channel.send("FAQ reset successfull")
+
+client.run(os.environ['TOKEN'])
